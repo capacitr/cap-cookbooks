f549861ea7a729408c48e464ec795f654ccd7507
patrick aubin <paubin12@gmail.com>
2012-11-13 11:08:13 -0600

    updated cookbooks for glenmark

--- capacitr/recipes/default.rb
+++ capacitr/recipes/default.rb
@@ -75,23 +75,7 @@ execute "virtualenv /home/%s/venv" % node["new_user"] do
     returns [0,1]
 end
 
-python_packages = [
-    "Django==1.4.2",
-    "pyyaml",
-    "PIL==1.1.7",
-    "MySQL-python",
-    "South==0.7.6",
-    "argparse==1.2.1",
-    "cropresize==0.1.6",
-    "distribute==0.6.30",
-    "django-autocomplete==1.0.dev49",
-    "django-thumbnail-works==0.2.3",
-    "gunicorn==0.15.0",
-    "wsgiref==0.1.2",
-    "-e git+https://github.com/tschellenbach/Django-facebook.git#egg=django_facebook"
-    ]
-
-python_packages.each do |package|
+node[:python_packages].each do |package|
     execute "pip install #{package}" do
         action :run
         cwd "/home/%s/site" % node[:new_user]
@@ -121,21 +105,12 @@ service "supervisor" do
     action :restart
 end
 
-execute "mysql -u root -ppassword -e \"create database \`%s\`\"" % node["dbname"] do
+execute "mysql -u root -ppassword -e \"create database %s\"" % node["dbname"] do
     action :run
     returns [0, 1]
 end
 
-execute "mysql -u root -ppassword -e \"set sql_mode='ANSI_QUOTES'\"" do
-    action :run
-end
-
-#execute "mysql -u root -ppassword -e 'GRANT ALL ON \"%s\".* TO \"%s\"@localhost IDENTIFIED BY \"%s\";'" % [node[:dbname], node[:dbuser], node[:dbpass]] do
-execute "mysql -u root -ppassword -e 'GRANT ALL ON `94e6037d`.* TO \"94e6037d\"@localhost IDENTIFIED BY \"94e6037a-08c2-40a0-8247\";'" do
-    action :run
-end
-
-execute "echo \"127.0.0.1 mysql.sharinthegrooves.com\" >> /etc/hosts" do 
+execute "mysql -u root -ppassword -e 'GRANT ALL ON \`%s\`.* TO \`%s\`@localhost IDENTIFIED BY \"%s\";'" % [node[:dbname], node[:dbuser], node[:dbpass]] do
     action :run
 end
 
@@ -153,6 +128,7 @@ execute "python manage.py migrate --database=%s" % node[:dbhost] do
     environment ({'PATH' => '/home/%s/venv/bin' % node["new_user"]})
     user node["new_user"]
     group node["new_user"]
+    returns [0,1]
 end
 
 execute "python manage.py loaddata fixtures/sites.yaml --database=%s" % node[:dbhost] do
