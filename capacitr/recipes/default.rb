8cd7e07c54272a6e207da997bca164db6d3e3f46
patrick aubin <paubin12@gmail.com>
2012-11-14 10:50:32 -0600

    cleaned up the default recipe, loaded dynamic fixtures

--- capacitr/recipes/default.rb
+++ capacitr/recipes/default.rb
@@ -1,26 +1,26 @@
 user node["new_user"] do
-    home "/home/%s" % node["new_user"]
+    home "/home/#{node[:new_user]}"
     shell "/bin/zsh"
     action :create
 end
 
-directory "/home/%s" % node["new_user"] do
+directory "/home/#{node[:new_user]}" do
     owner node["new_user"] 
     group node["new_user"]
 end
 
-directory "/home/%s/static" % node["new_user"] do
+directory "/home/#{node[:new_user]}/static" do
     owner node["new_user"] 
     group node["new_user"]
 end
 
-link "/home/%s/site" % node["new_user"] do
+link "/home/#{node[:new_user]}/site" do
     to "/vagrant"
     owner node["new_user"]
     group node["new_user"]
 end
 
-directory "/home/%s/uploads" % node["new_user"] do
+directory "/home/#{node[:new_user]}/uploads" do
     owner node["new_user"] 
     group node["new_user"]
 end
@@ -68,7 +68,7 @@ execute "easy_install virtualenv" do
     action :run
 end
 
-execute "virtualenv /home/%s/venv" % node["new_user"] do
+execute "virtualenv /home/#{node[:new_user]}/venv" do
     action :run
     user node[:new_user]
     group node[:new_user]
@@ -78,26 +78,26 @@ end
 node[:python_packages].each do |package|
     execute "pip install #{package}" do
         action :run
-        cwd "/home/%s/site" % node[:new_user]
+        cwd "/home/#{node[:new_user]}/site"
         user node[:new_user]
         group node[:new_user]
-        environment ({'PATH' => '/home/%s/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' % node["new_user"]})
+        environment ({'PATH' => "/home/#{node[:new_user]}/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"})
     end
 end
 
-template "/etc/nginx/sites-available/%s.conf" % node["new_user"] do
+template "/etc/nginx/sites-available/#{node[:new_user]}.conf" do
     source "site.conf.erb"
 end
 
-link "/etc/nginx/sites-enabled/%s.conf" % node["new_user"] do
-    to "/etc/nginx/sites-available/%s.conf" % node["new_user"]
+link "/etc/nginx/sites-enabled/#{node[:new_user]}.conf" do
+    to "/etc/nginx/sites-available/#{node[:new_user]}.conf"
 end
 
 service "nginx" do
     action :restart
 end
 
-template "/etc/supervisor/conf.d/%s.conf" % node["new_user"] do
+template "/etc/supervisor/conf.d/#{node[:new_user]}.conf" do
     source "supervisor.conf.erb"
 end
 
@@ -105,52 +105,47 @@ service "supervisor" do
     action :restart
 end
 
-execute "mysql -u root -ppassword -e \"create database %s\"" % node["dbname"] do
+execute "mysql -u root -ppassword -e \"create database #{node[:dbname]}\"" do
     action :run
     returns [0, 1]
 end
 
-execute "mysql -u root -ppassword -e 'GRANT ALL ON \`%s\`.* TO \`%s\`@localhost IDENTIFIED BY \"%s\";'" % [node[:dbname], node[:dbuser], node[:dbpass]] do
+execute "mysql -u root -ppassword -e 'GRANT ALL ON \`#{node[:dbname]}\`.* TO \`#{node[:dbuser]}\`@localhost IDENTIFIED BY \"#{node[:dbpass]}\";'" do
     action :run
 end
 
-execute "python manage.py syncdb --noinput --database=%s" % node[:dbhost] do
+execute "python manage.py syncdb --noinput --database=#{node[:dbhost]}" do
     action :run
-    cwd "/home/%s/site" % node["new_user"]
-    environment ({'PATH' => '/home/%s/venv/bin:/usr/bin' % node["new_user"]})
+    cwd "/home/#{node[:new_user]}/site"
+    environment ({'PATH' => "/home/#{node[:new_user]}/venv/bin:/usr/bin"})
     user node["new_user"]
     group node["new_user"]
 end
 
-execute "python manage.py migrate --database=%s" % node[:dbhost] do
+execute "python manage.py migrate --database=#{node[:dbhost]}" do
     action :run
-    cwd "/home/%s/site" % node["new_user"]
-    environment ({'PATH' => '/home/%s/venv/bin' % node["new_user"]})
+    cwd "/home/#{node[:new_user]}/site"
+    environment ({'PATH' => "/home/#{node[:new_user]}/venv/bin"})
     user node["new_user"]
     group node["new_user"]
     returns [0,1]
 end
 
-execute "python manage.py loaddata fixtures/sites.yaml --database=%s" % node[:dbhost] do
-    action :run
-    cwd "/home/%s/site" % node["new_user"]
-    environment ({'PATH' => '/home/%s/venv/bin' % node["new_user"]})
-    user node["new_user"]
-    group node["new_user"]
-end
 
-execute "python manage.py loaddata fixtures/admin.yaml --database=%s" % node[:dbhost] do
-    action :run
-    cwd "/home/%s/site" % node["new_user"]
-    environment ({'PATH' => '/home/%s/venv/bin' % node["new_user"]})
-    user node["new_user"]
-    group node["new_user"]
+node[:fixtures].each do |fixture|
+    execute "python manage.py loaddata fixtures/#{fixture} --database=#{node[:dbhost]}" do
+        action :run
+        cwd "/home/#{node[:new_user]}/site"
+        environment ({'PATH' => "/home/#{node[:new_user]}/venv/bin"})
+        user node["new_user"]
+        group node["new_user"]
+    end
 end
 
 execute "python manage.py collectstatic --noinput" do
     action :run
-    cwd "/home/%s/site" % node["new_user"]
-    environment ({'PATH' => '/home/%s/venv/bin' % node["new_user"]})
+    cwd "/home/#{node[:new_user]}/site"
+    environment ({'PATH' => "/home/#{node[:new_user]}/venv/bin"})
     user node["new_user"]
     group node["new_user"]
 end
