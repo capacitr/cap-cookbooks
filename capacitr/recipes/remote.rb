455ba7b36434f735d0e6ca77827bb039e1b27d24
patrick aubin <patrick@capacitr.com>
2013-01-21 17:13:04 -0600

    updated remote cookbook

--- capacitr/recipes/remote.rb
+++ capacitr/recipes/remote.rb
@@ -1,118 +1,128 @@
-user node["new_user"] do
-    home "/home/#{node[:new_user]}"
-    shell "/bin/bash"
-    action :create
-end
+node["users"].each do |new_user|
+    user new_user[:username] do
+        home "/home/#{new_user[:username]}"
+        shell "/bin/bash"
+        action :create
+    end
 
-directory "/home/#{node[:new_user]}/uploads" do
-    owner node["new_user"] 
-    group node["new_user"]
-end
+    directory "/home/#{new_user[:username]}" do
+        owner new_user[:username]
+        group new_user[:username]
+    end
 
-execute "chown -R #{node[:new_user]}:#{node[:new_user]} /home/#{node[:new_user]}" do
-    action :run
-end
+    directory "/home/#{new_user[:username]}/uploads" do
+        owner new_user[:username]
+        group new_user[:username]
+    end
 
-execute "apt-get update" do
-    action :run
-end
+    execute "chown -R #{new_user[:username]}:#{new_user[:username]} /home/#{new_user[:username]}" do
+        action :run
+    end
 
-execute "echo mysql-server mysql-server/root_password password password |
-debconf-set-selections" do
-    action :run
-end
+    template "/etc/supervisor/conf.d/{new_user[:username]}.conf" do
+        source "supervisor.conf.erb"
+        variables({
+            :user => new_user[:username],
+            :port => new_user[:port],
+            :domains => new_user[:domains]
+        })
+    end
 
-execute "echo mysql-server mysql-server/root_password_again password password |
-debconf-set-selections" do
-    action :run
-end
+    execute "supervisorctl reread" do
+        action :run
+    end
+#
+#    execute "mysql -u root -ppassword -e \"create database \\\`{new_user[:dbname]}\\\`\"" do
+#        action :run
+#        returns [0,1]
+#    end
+#
+#    execute "mysql -u root -ppassword -e 'GRANT ALL ON \`{new_user[:dbname]}\`.* TO \`{new_user[:dbuser]}\`@localhost IDENTIFIED BY \"{new_user[:dbpass]}\";'" do
+#        action :run
+#    end
 
-execute "apt-get install -qfy" do
-    action :run
-end
+    template "/etc/nginx/sites-available/#{new_user[:username]}.conf" do
+        source "site.conf.erb"
+        variables({
+            :user => new_user[:username],
+            :port => new_user[:port],
+            :domains => new_user[:domains]
+        })
+    end
 
-apt_packages = [
-    "python-setuptools",
-    "nginx",
-    "python-dev",
-    "libmysqlclient-dev",
-    "mysql-server",
-    "zlib1g-dev", 
-    "libxml2",
-    "libxslt1.1",
-    "libxml2-dev",
-    "libxslt1-dev",
-    "libfreetype6-dev", 
-    "liblcms1-dev",
-    "libjpeg62-dev",
-    "supervisor",
-    "memcached",
-    "libcurl3",
-    "libcurl3-gnutls",
-    "git-core",
-    "unzip",
-    "make",
-    "ruby",
-    "rubygems",
-    "openjdk-6-jre-headless",
-]
+    link "/etc/nginx/sites-enabled/#{new_user[:username]}.conf" do
+        to "/etc/nginx/sites-available/#{new_user[:username]}.conf"
+    end
 
-
-apt_packages.each do |package|
-    execute "apt-get install -yq #{package}" do
-        action :run 
+    service "nginx" do
+        action :reload
     end
 end
+
 #
-#execute "wget -O /root/compiler-latest.zip http://closure-compiler.googlecode.com/files/compiler-latest.zip" do
+#execute "apt-get update" do
 #    action :run
 #end
 #
-#execute "unzip /root/compiler-latest.zip -d /root/compiler-latest" do
+#execute "apt-get install -qfy" do
 #    action :run
-#    user "root"
-#    group "root"
 #end
 #
-#execute "mv /root/compiler-latest/compiler.jar /usr/bin/closure_compiler" do
-#    action :run
-#    user "root"
-#    group "root"
+#apt_packages = [
+#    "python-setuptools",
+#    "nginx",
+#    "python-dev",
+#    "libmysqlclient-dev",
+#    "mysql-server",
+#    "zlib1g-dev", 
+#    "libxml2",
+#    "libxslt1.1",
+#    "libxml2-dev",
+#    "libxslt1-dev",
+#    "libfreetype6-dev", 
+#    "liblcms1-dev",
+#    "libjpeg62-dev",
+#    "supervisor",
+#    "memcached",
+#    "libcurl3",
+#    "libcurl3-gnutls",
+#    "git-core",
+#    "unzip",
+#    "make",
+#    "ruby",
+#    "rubygems",
+#    "openjdk-6-jre-headless",
+#]
+#
+#
+#apt_packages.each do |package|
+#    execute "apt-get install -yq #{package}" do
+#        action :run 
+#    end
 #end
 #
-#file "/usr/bin/closure_compiler" do
-#    mode 00755
-#    owner "root"
-#    group "root"
-#end
+##
+##execute "wget -O /root/compiler-latest.zip http://closure-compiler.googlecode.com/files/compiler-latest.zip" do
+##    action :run
+##end
+##
+##execute "unzip /root/compiler-latest.zip -d /root/compiler-latest" do
+##    action :run
+##    user "root"
+##    group "root"
+##end
+##
+##execute "mv /root/compiler-latest/compiler.jar /usr/bin/closure_compiler" do
+##    action :run
+##    user "root"
+##    group "root"
+##end
+##
+##file "/usr/bin/closure_compiler" do
+##    mode 00755
+##    owner "root"
+##    group "root"
+##end
+##
 #
-
-template "/etc/nginx/sites-available/#{node[:new_user]}.conf" do
-    source "site.conf.erb"
-end
-
-link "/etc/nginx/sites-enabled/#{node[:new_user]}.conf" do
-    to "/etc/nginx/sites-available/#{node[:new_user]}.conf"
-end
-
-service "nginx" do
-    action :reload
-end
-
-template "/etc/supervisor/conf.d/#{node[:new_user]}.conf" do
-    source "supervisor.conf.erb"
-end
-
-service "supervisor" do
-    action :restart
-end
-
-execute "mysql -u root -ppassword -e \"create database \\\`#{node[:dbname]}\\\`\"" do
-    action :run
-    returns [0,1]
-end
-
-execute "mysql -u root -ppassword -e 'GRANT ALL ON \`#{node[:dbname]}\`.* TO \`#{node[:dbuser]}\`@localhost IDENTIFIED BY \"#{node[:dbpass]}\";'" do
-    action :run
-end
 
