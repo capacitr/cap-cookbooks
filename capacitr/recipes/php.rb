81aaf5a7990c0c78dfb35c005e5024b12dbde7f9
patrick aubin <paubin12@gmail.com>
2013-03-11 17:11:52 -0500

    added generic app

--- capacitr/recipes/php.rb
+++ capacitr/recipes/php.rb
@@ -42,34 +42,6 @@ execute "apt-get update" do
     action :run
 end
 
-execute "easy_install virtualenv" do
-    action :run
-end
-
-execute "virtualenv --distribute /home/#{username}/venv" do
-    action :run
-    user username
-    group username
-    returns [0,1]
-end
-
-unless File.file?("requirements.txt")
-  execute "pip install -v --no-index --index-url=file://basket/ -r requirements.txt" do
-      action :run
-      cwd "/home/#{username}/site"
-      user "root"
-      group "root" 
-      environment ({'PATH' => "/home/#{username}/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"})
-      returns [0,1]
-  end
-end
-
-execute "chown -R #{username}:#{username} /home/#{username}/venv" do
-    action :run
-    user "root"
-    group "root"
-end
-
 execute "mysql -u root -ppassword -e \"create database \\\`#{dbname}\\\`\"" do
     action :run
     returns [0,1]
@@ -87,15 +59,6 @@ template "/etc/nginx/sites-available/#{username}" do
         :domains => domains,
         :port => "8000"
     })
-end
-
-template "/etc/supervisor/conf.d/#{username}.conf" do
-    variables({
-        :user => username,
-        :port => "8000",
-        :domains => domains
-    })
-    source "supervisor.conf.erb"
 end
 
 link "/etc/nginx/sites-enabled/#{username}" do
